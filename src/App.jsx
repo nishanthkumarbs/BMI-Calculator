978b0dfba490183355d96093063c3cb32f802948
nishanthkumarbs <nishanthgowda014@gmail.com>
2026-01-11 18:44:16 +0530

    Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
+import Calculator from './Components/Calculator/Calculator'
 
 const App = () => {
     return (
         <div>
             <Navbar />
             <Home />
+            <Calculator />
         </div>
     )
 }
---
+++ src/Components/Calculator/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Calculator.css'
+
+const Calculator = () => {
+    return (
+        <>
+        <h1>BMI Calculator</h1>
+        </>
+    )
+}
+export default Calculator
