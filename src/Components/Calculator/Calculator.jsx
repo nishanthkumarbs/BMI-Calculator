22a2238573202b6d0b96b420acc4f439043b4eb0
nishanthkumarbs <nishanthgowda014@gmail.com>
2026-01-12 22:00:42 +0530

    Calculator

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -4,7 +4,33 @@ import './Calculator.css'
 const Calculator = () => {
     return (
         <>
-        <h1>BMI Calculator</h1>
+            <section className='container-main'>
+                <aside className='container-form'>
+                    <div className='fromContainer1'>
+                        <h1>BMI Calculator</h1>
+                        <form action="">
+                            <label htmlFor="age">Age: </label>
+                            <input type="number" id='age'  required />
+                            <br />
+                            <label htmlFor="height">Height: </label>
+                            <input type="number" id='height' required />
+                            <br />
+                            <label htmlFor="weight">Weight: </label>
+                            <input type="number" id='weight'/>
+                            <br />
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                    <div className="formContainer2">
+                        <h1>Form 2</h1>
+                    </div>
+                </aside>
+                <aside className="container-table">
+                    <div className="tableContainer">
+                        <h1>TABLE</h1>
+                    </div>
+                </aside>
+            </section>
         </>
     )
 }
